f3983067122ea0d8d767c2649802b046f528e9a0
Jayesh Jain <138527414+jayesh-code@users.noreply.github.com>
2024-05-19 20:10:40 +0530

    Update rps.py

--- rps.py
+++ rps.py
@@ -6,6 +6,7 @@ computer_wins = 0
 options = ["rock", "paper", "scissors"]
 
 while True:
+    print("__________________________________________________")
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
         break
